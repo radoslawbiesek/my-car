ae7e5ad2238593639ed79f5702855e48450eb6d2
Radosław Biesek <radoslaw.biesek@gmail.com>
2023-07-02 17:17:45 +0200

    init fuel usage

--- src/utils/fuel.ts
+++ src/utils/fuel.ts
@@ -4,3 +4,14 @@ export function calculateCost(cost: number, vatDeduction: number) {
   const multiplier = (1 + VAT * (1 - vatDeduction)) / (1 + VAT);
   return parseFloat((cost * multiplier).toFixed(2));
 }
+
+export function calculateFuelUsage(rows: { mileage: number; amount: number }[]) {
+  if (rows.length < 2) {
+    return 0;
+  }
+
+  const totalMileage = rows.at(-1)!.mileage - rows[0]!.mileage;
+  const totalAmount = rows.slice(1).reduce((total, row) => total + row.amount, 0);
+
+  return parseFloat(((totalAmount / totalMileage) * 100).toFixed(2));
+}
